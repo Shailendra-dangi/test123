d956b917d97835b0630bbc9d79bb3d00369145bc
SHAILENDRA1320 <shailendrasingh1320@gmail.com>
2026-01-27 22:13:40 +0530

    Faulty Calculator

---
+++ 59+Harry/script.js
@@ -0,0 +1,24 @@
+let a = Number(prompt("Inter your value a "));
+let b = Number(prompt("Inter your value b "));
+
+let random = Math.random();
+
+let Addition = a + b;
+let Subtraction = a - b;
+let Multiplication = a * b;
+let Division = a / b;
+let Exponentiation = a ** b;
+
+if (random < 0.1) {
+  console.log("Addition Calculation:", a - b);
+  console.log("Subtraction Calculation:", a / b);
+  console.log("Multipication Calculation:", a + b);
+  console.log("Division Calculation:", a ** b);
+  console.log("Exponentiaton Calculation:", a * b);
+} else {
+  console.log("Addition Calculation:", a + b);
+  console.log("Subtraction Calculation:", a - b);
+  console.log("Multipication Calculation:", a * b);
+  console.log("Division Calculation:", a / b);
+  console.log("Exponentiaton Calculation:", a ** b);
+}
